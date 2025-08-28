4c87786f78fafbe2d279392ba1d169b4d4010d01
Ganga Singh <gangasingh1734@gmail.com>
2025-08-28 13:15:43 +0530

    change in th ports

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import mainRouter from './routes/index.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
