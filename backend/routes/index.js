df23aaa42da9018ce7e047cd8b995a7d9eb048f5
Ganga Singh <gangasingh1734@gmail.com>
2025-08-28 14:13:17 +0530

    Added the real time stats feature

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ import authrouter from "./auth.routes.js";
 import classRouter from "./class.router.js"
 import subjectRouter from "./subject.routes.js"
 import resourceRouter from "./resource.routes.js"
+import statsRouter from "./stats.routes.js" 
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/auth/student",authrouter);
 router.use("/class",classRouter);
 router.use("/subject",subjectRouter)
 router.use("/resource",resourceRouter)
+router.use("/stats",statsRouter)
 
 export default router;
